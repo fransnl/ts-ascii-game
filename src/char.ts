a12dff1441fa60449c98f5875151398c93e2df15
fransnl <nl.frans@gmail.com>
2023-03-16 22:38:41 +0100

    started refactor

---
+++ src/char.ts
@@ -0,0 +1,9 @@
+export const player {
+    x: 400,
+    y: 400,
+    playerWorldPos: [20, 20],
+    updatePos: (x, y) => {
+        x = this.x;
+        y = this.y;
+    }
+}
--- src/components/game.ts
+++
@@ -1,5 +0,0 @@
-let c: HtmlCanvasElement = document.getElementById('game');
-let ctx: CanvasRenderingConstext2D = c.getContext('2D')!;
-
-ctx.font = "30px Arial";
-ctx.fillText('O', 100, 50);
